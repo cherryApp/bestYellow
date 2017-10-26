06af5dd840162ac115e39578eb77cf180bca38c0
cherryApp <j.cserko@gmail.com>
2017-10-26 16:38:07 +0200

    Feat: add homework.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-console.log("Hello Yelow7!");
+function getJson(url, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("get", url);
+
+  xhr.onload = function(ev) {
+    var footballData = JSON.parse(ev.target.response);
+    callback(footballData);
+  };
+  xhr.send();
+}
